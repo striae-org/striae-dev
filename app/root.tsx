e0d95aa8992f9a409a20e8b442b9ef9dcfab1b53
Stephen J. Lu <Stephen@StephenJLu.com>
2025-01-09 13:39:06 -0800

    fixed titles

--- app/root.tsx
+++ app/root.tsx
@@ -37,13 +37,12 @@ interface DocumentProps {
   title?: string;
 }
 
-function Document({ children, title }: DocumentProps) {
+function Document({ children }: DocumentProps) {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        {title && <title>{title}</title>}
+        <meta name="viewport" content="width=device-width, initial-scale=1" />        
         <Meta />
         <Links />
       </head>
