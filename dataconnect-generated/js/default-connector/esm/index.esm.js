fe01a04863988956852b63f67bd3e0467552d6bb
Stephen J. Lu <Stephen@StephenJLu.com>
2025-01-11 08:47:15 -0800

    firebase dataconnect init

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { getDataConnect, validateArgs } from 'firebase/data-connect';
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'striae',
+  location: 'us-west1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,9 @@
+const { getDataConnect, validateArgs } = require('firebase/data-connect');
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'striae',
+  location: 'us-west1'
+};
+exports.connectorConfig = connectorConfig;
+
