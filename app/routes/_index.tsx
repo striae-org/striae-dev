6f1ce57cff8e5dbc470fbef235cf86da97d6f977
Stephen J. Lu <Stephen@StephenJLu.com>
2025-01-11 18:36:39 -0800

    updated index.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,1 +1,9 @@
-export { Login as default, meta, loader } from './auth/login'
+import { Login, meta, loader } from './auth/login'
+
+export { meta, loader }
+
+export default function App() {
+    return (
+        <Login />
+    );
+}
