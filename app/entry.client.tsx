0050ae4e44384584bc736b91c39863f969a6acab
Stephen J. Lu <Stephen@StephenJLu.com>
2025-01-11 20:27:08 -0800

    restored scripts

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -2,13 +2,9 @@ import { RemixBrowser } from "@remix-run/react";
 import { startTransition } from "react";
 import { hydrateRoot } from "react-dom/client";
 
-if (process.env.NODE_ENV === 'production') {
-  console.log('Hydration disabled');
-} else {
-  startTransition(() => {
-    hydrateRoot(
-      document,
-      <RemixBrowser />
-    );
-  });
-}
+startTransition(() => {
+  hydrateRoot(
+    document,    
+      <RemixBrowser />    
+  );
+});
