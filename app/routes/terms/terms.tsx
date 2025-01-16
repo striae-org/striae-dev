6148874d1894f31541e777aa3cf601e134043d84
Stephen J. Lu <Stephen@StephenJLu.com>
2025-01-16 12:09:52 -0800

    moved storage id to heading

--- app/routes/terms/terms.tsx
+++ app/routes/terms/terms.tsx
@@ -206,8 +206,8 @@ You agree that the original English text shall prevail in the case of a dispute.
 <p>We reserve the right, at Our sole discretion, to modify or replace these Terms at any time. If a revision is material We will make reasonable efforts to provide at least 30 days' notice prior to any new terms taking effect. What constitutes a material change will be determined at Our sole discretion.</p>
 <p>By continuing to access or use Our Service after those revisions become effective, You agree to be bound by the revised terms. If You do not agree to the new terms, in whole or in part, please stop using the website and the Service.</p>
         </section>
-        <section id="storage" className={styles.section}>
-          <h2>Data Storage Addendum</h2>
+        <section className={styles.section}>
+          <h2 id="storage">Data Storage Addendum</h2>
 <p>At AllyForensics, we take Your privacy and Data security seriously. We understand the sensitive nature of the Data that You may store with Us. This Data Storage Addendum outlines how we handle and store Your Data within the app.</p>
 <ul>
   <li>
