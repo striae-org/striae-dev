9b68549e18bb3838b35d690350b656c166825193
Stephen J. Lu <Stephen@StephenJLu.com>
2025-08-24 15:40:52 -0700

    fixed pdf links

--- app/routes/privacy/privacy.tsx
+++ app/routes/privacy/privacy.tsx
@@ -19,7 +19,7 @@ export const Privacy = () => {
       <div className={styles.content}>
         <h1>Privacy Policy</h1>
         <p className={styles.lastUpdated}>Last updated: August 24, 2025</p>
-        <p><Link to="/striae-pp-plain.pdf" download="Striae-Privacy-Policy-Summary.pdf">Download Summary PDF</Link></p>
+        <p><a href="/striae-pp-plain.pdf" download="Striae-Privacy-Policy-Summary.pdf">Download Summary PDF</a></p>
         <p>This Privacy Policy describes Our policies and procedures on the collection, use and disclosure of Your information when You use the Service and tells You about Your privacy rights and how the law protects You.</p>
 <p>We use Your Personal data to provide and improve the Service. By using the Service, You agree to the collection and use of information in accordance with this Privacy Policy.</p>
         
