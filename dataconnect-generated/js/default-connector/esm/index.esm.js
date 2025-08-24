f35e3afafc8f864ce3cf4db747d4bdf0c634b9b8
Stephen J. Lu <Stephen@StephenJLu.com>
2025-08-23 21:27:37 -0700

    rm dataconnect

--- dataconnect-generated/js/default-connector/esm/index.esm.js
+++
@@ -1,8 +0,0 @@
-import { , validateArgs } from 'firebase/data-connect';
-
-export const connectorConfig = {
-  connector: 'default',
-  service: 'striae',
-  location: 'us-west1'
-};
-
--- dataconnect-generated/js/default-connector/index.cjs.js
+++
@@ -1,9 +0,0 @@
-const { , validateArgs } = require('firebase/data-connect');
-
-const connectorConfig = {
-  connector: 'default',
-  service: 'striae',
-  location: 'us-west1'
-};
-exports.connectorConfig = connectorConfig;
-
