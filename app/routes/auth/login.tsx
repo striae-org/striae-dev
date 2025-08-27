0ac18c33d57b506b1e2d82b4cce87e9757805bff
Stephen J. Lu <Stephen@StephenJLu.com>
2025-08-26 19:36:13 -0700

    eye icons for pwds

--- app/routes/auth/login.tsx
+++ app/routes/auth/login.tsx
@@ -334,13 +334,27 @@ export const Login = () => {
                       aria-label={showPassword ? "Hide password" : "Show password"}
                     >
                       {showPassword ? (
-                        <svg width="24" height="24" viewBox="0 0 24 24">
-                          <path d="M12 6c3.98 0 7.88 2.35 9.54 6-1.09 2.29-3.05 4.1-5.45 4.83l1.46 1.46c2.97-1.13 5.22-3.66 6.12-6.56C21.13 7.67 16.84 4.5 12 4.5c-1.43 0-2.8.19-4.1.54l1.45 1.45C10.13 6.08 11.04 6 12 6zm10.54 12.88l-1.41 1.41-1.44-1.44C17.11 19.6 14.66 21 12 21c-5.25 0-9.45-3.28-11-7.5 1.03-2.63 3-4.81 5.56-6.2l-1.42-1.42 1.41-1.41 18 18z"/>
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <g clipPath="url(#clip0_7_33)">
+                            <path d="M17.94 17.94C16.2306 19.243 14.1491 19.9649 12 20C5 20 1 12 1 12C2.24389 9.6819 3.96914 7.65661 6.06 6.06M9.9 4.24C10.5883 4.07888 11.2931 3.99834 12 4C19 4 23 12 23 12C22.393 13.1356 21.6691 14.2047 20.84 15.19M14.12 14.12C13.8454 14.4147 13.5141 14.6512 13.1462 14.8151C12.7782 14.9791 12.3809 15.0673 11.9781 15.0744C11.5753 15.0815 11.1752 15.0074 10.8016 14.8565C10.4281 14.7056 10.0887 14.481 9.80385 14.1962C9.51897 13.9113 9.29439 13.5719 9.14351 13.1984C8.99262 12.8248 8.91853 12.4247 8.92563 12.0219C8.93274 11.6191 9.02091 11.2218 9.18488 10.8538C9.34884 10.4859 9.58525 10.1546 9.88 9.88M1 1L23 23" stroke="#1E1E1E" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"/>
+                          </g>
+                          <defs>
+                            <clipPath id="clip0_7_33">
+                              <rect width="24" height="24" fill="white"/>
+                            </clipPath>
+                          </defs>
                         </svg>
                       ) : (
-                        <svg width="24" height="24" viewBox="0 0 24 24">
-                          <path d="M12,4.5C7,4.5,2.73,7.61,1,12c1.73,4.39,6,7.5,11,7.5s9.27-3.11,11-7.5C21.27,7.61,17,4.5,12,4.5z M12,17c-2.76,0-5-2.24-5-5s2.24-5,5-5s5,2.24,5,5S14.76,17,12,17z"/>
-                          <circle cx="12" cy="12" r="2.5"/>
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <g clipPath="url(#clip0_7_18)">
+                            <path d="M1 12C1 12 5 4 12 4C19 4 23 12 23 12C23 12 19 20 12 20C5 20 1 12 1 12Z" stroke="#1E1E1E" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"/>
+                            <path d="M12 15C13.6569 15 15 13.6569 15 12C15 10.3431 13.6569 9 12 9C10.3431 9 9 10.3431 9 12C9 13.6569 10.3431 15 12 15Z" stroke="#1E1E1E" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"/>
+                          </g>
+                          <defs>
+                            <clipPath id="clip0_7_18">
+                              <rect width="24" height="24" fill="white"/>
+                            </clipPath>
+                          </defs>
                         </svg>
                       )}
                     </button>
@@ -365,13 +379,27 @@ export const Login = () => {
                           aria-label={showConfirmPassword ? "Hide confirm password" : "Show confirm password"}
                         >
                           {showConfirmPassword ? (
-                            <svg width="24" height="24" viewBox="0 0 24 24">
-                              <path d="M12 6c3.98 0 7.88 2.35 9.54 6-1.09 2.29-3.05 4.1-5.45 4.83l1.46 1.46c2.97-1.13 5.22-3.66 6.12-6.56C21.13 7.67 16.84 4.5 12 4.5c-1.43 0-2.8.19-4.1.54l1.45 1.45C10.13 6.08 11.04 6 12 6zm10.54 12.88l-1.41 1.41-1.44-1.44C17.11 19.6 14.66 21 12 21c-5.25 0-9.45-3.28-11-7.5 1.03-2.63 3-4.81 5.56-6.2l-1.42-1.42 1.41-1.41 18 18z"/>
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                              <g clipPath="url(#clip0_7_33_confirm)">
+                                <path d="M17.94 17.94C16.2306 19.243 14.1491 19.9649 12 20C5 20 1 12 1 12C2.24389 9.6819 3.96914 7.65661 6.06 6.06M9.9 4.24C10.5883 4.07888 11.2931 3.99834 12 4C19 4 23 12 23 12C22.393 13.1356 21.6691 14.2047 20.84 15.19M14.12 14.12C13.8454 14.4147 13.5141 14.6512 13.1462 14.8151C12.7782 14.9791 12.3809 15.0673 11.9781 15.0744C11.5753 15.0815 11.1752 15.0074 10.8016 14.8565C10.4281 14.7056 10.0887 14.481 9.80385 14.1962C9.51897 13.9113 9.29439 13.5719 9.14351 13.1984C8.99262 12.8248 8.91853 12.4247 8.92563 12.0219C8.93274 11.6191 9.02091 11.2218 9.18488 10.8538C9.34884 10.4859 9.58525 10.1546 9.88 9.88M1 1L23 23" stroke="#1E1E1E" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"/>
+                              </g>
+                              <defs>
+                                <clipPath id="clip0_7_33_confirm">
+                                  <rect width="24" height="24" fill="white"/>
+                                </clipPath>
+                              </defs>
                             </svg>
                           ) : (
-                            <svg width="24" height="24" viewBox="0 0 24 24">
-                              <path d="M12,4.5C7,4.5,2.73,7.61,1,12c1.73,4.39,6,7.5,11,7.5s9.27-3.11,11-7.5C21.27,7.61,17,4.5,12,4.5z M12,17c-2.76,0-5-2.24-5-5s2.24-5,5-5s5,2.24,5,5S14.76,17,12,17z"/>
-                              <circle cx="12" cy="12" r="2.5"/>
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                              <g clipPath="url(#clip0_7_18_confirm)">
+                                <path d="M1 12C1 12 5 4 12 4C19 4 23 12 23 12C23 12 19 20 12 20C5 20 1 12 1 12Z" stroke="#1E1E1E" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"/>
+                                <path d="M12 15C13.6569 15 15 13.6569 15 12C15 10.3431 13.6569 9 12 9C10.3431 9 9 10.3431 9 12C9 13.6569 10.3431 15 12 15Z" stroke="#1E1E1E" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"/>
+                              </g>
+                              <defs>
+                                <clipPath id="clip0_7_18_confirm">
+                                  <rect width="24" height="24" fill="white"/>
+                                </clipPath>
+                              </defs>
                             </svg>
                           )}
                         </button>
