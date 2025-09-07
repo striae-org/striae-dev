004ea98c8dfee927d5dcef0986dec85e6f33cd33
Stephen J. Lu <Stephen@StephenJLu.com>
2025-09-07 13:45:00 -0700

    Revert "session .env dev"
    This reverts commit 8f126055195a965a78e8567b1877e41b54e85ca8.

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import {
   vitePlugin as remix,
   cloudflareDevProxyVitePlugin as remixCloudflareDevProxy,
 } from "@remix-run/dev";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 declare module "@remix-run/cloudflare" {
@@ -11,29 +11,21 @@ declare module "@remix-run/cloudflare" {
   }
 }
 
-export default defineConfig(({ mode }) => {
-  // Load environment variables
-  const env = loadEnv(mode, process.cwd(), '');
-  
-  return {
-    server: {
-      port: 7777,
-    },
-    define: {
-      'process.env.SESSION_SECRET': JSON.stringify(env.SESSION_SECRET),
-    },
-    plugins: [
-      remixCloudflareDevProxy(),
-      remix({      
-        future: {
-          v3_fetcherPersist: true,
-          v3_relativeSplatPath: true,
-          v3_throwAbortReason: true,
-          v3_singleFetch: true,
-          v3_lazyRouteDiscovery: true,
-        },
-      }),
-      tsconfigPaths(),
-    ],
-  };
+export default defineConfig({
+  server: {
+    port: 7777,
+  },
+  plugins: [
+    remixCloudflareDevProxy(),
+    remix({      
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+        v3_singleFetch: true,
+        v3_lazyRouteDiscovery: true,
+      },
+    }),
+    tsconfigPaths(),
+  ],
 });
