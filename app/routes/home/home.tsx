cc2a75c6561000bb54f0502dd9c2e7326aa2c2f6
Stephen J. Lu <Stephen@StephenJLu.com>
2026-02-15 10:23:39 -0700

    Revert "rm access route"
    This reverts commit 76375b69928bb58d2d0a23194f7f31142d050a26.

--- app/routes/home/home.tsx
+++ app/routes/home/home.tsx
@@ -53,12 +53,18 @@ export default function Home() {
             <p className={styles.noticeText}>Access to Striae is free for professionals employed at a forensic laboratory or organization
               <br />
               </p>
-            <div className={styles.buttonGroup}>              
+            <div className={styles.buttonGroup}>
+              <Link 
+                to="/access#top"
+                viewTransition
+                className={styles.actionButton}>
+                Agency Registration
+              </Link>
               <a 
                 href="https://support.striae.org/form/Y4sWjhi6Ppf"
                 target="_blank"
                 rel="noopener noreferrer"
-                className={styles.actionButton}>
+                className={styles.signInButton}>
                 Request a Demo
               </a>
             </div>            
