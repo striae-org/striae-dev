46a3bce25ce7c8ef0a6fe1d05629390f32285c6a
Stephen J. Lu <Stephen@StephenJLu.com>
2026-02-20 19:01:25 -0700

    update policy dates

--- app/routes/privacy/privacy.tsx
+++ app/routes/privacy/privacy.tsx
@@ -22,7 +22,7 @@ export const Privacy = () => {
       </Link>
       <div className={styles.content}>
         <h1>Privacy Policy</h1>
-        <p className={styles.lastUpdated}>Last updated: September 12, 2025</p>        
+        <p className={styles.lastUpdated}>Last updated: February 20, 2026</p>        
         <p>This Privacy Policy describes Our policies and procedures on the collection, use and disclosure of Your information when You use the Service and tells You about Your privacy rights and how the law protects You.</p>
         <p>We use Your Personal data to provide and improve the Service. By using the Service, You agree to the collection and use of information in accordance with this Privacy Policy.</p>
         <hr />
